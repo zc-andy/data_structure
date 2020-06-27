4f1cc631875601cab40fe50b9f9713ddf0fe338b
ZC1234567 <15905188618@163.com>
2020-06-27 16:17:47 +0800

    修改tab

--- gtest/GtestList.cpp
+++ gtest/GtestList.cpp
@@ -15,50 +15,50 @@ namespace
 
 class Demo
 {
-	public:
-		Demo() : m_iValue(0)
-		{}
+    public:
+        Demo() : m_iValue(0)
+        {}
 
-		~Demo()
-		{}
+        ~Demo()
+        {}
 
-	public:
-		int32_t m_iValue;
-		char m_ch;
+    public:
+        int32_t m_iValue;
+        char m_ch;
 };
 
 TEST_F(TestList, TEST_LIST)
 {
-	NS_LIST::List<Demo> m_List;
+    NS_LIST::List<Demo> m_List;
 
-	NS_LIST::List<Demo> *pNode1 = new NS_LIST::List<Demo>;
-	pNode1->m_list_data.m_iValue = 1;
-	pNode1->m_list_data.m_ch = 'A';
-	ASSERT_EQ(DS_OK, m_List.InsertNodeAtHead(pNode1));
-	NS_LIST::List<Demo> *pCur = pNode1;
+    NS_LIST::List<Demo> *pNode1 = new NS_LIST::List<Demo>;
+    pNode1->m_list_data.m_iValue = 1;
+    pNode1->m_list_data.m_ch = 'A';
+    ASSERT_EQ(DS_OK, m_List.InsertNodeAtHead(pNode1));
+    NS_LIST::List<Demo> *pCur = pNode1;
 
-	pNode1 = new NS_LIST::List<Demo>;
-	pNode1->m_list_data.m_iValue = 3;
-	pNode1->m_list_data.m_ch = 'C';
-	ASSERT_EQ(DS_OK, m_List.InsertNodeAtHead(pNode1));
+    pNode1 = new NS_LIST::List<Demo>;
+    pNode1->m_list_data.m_iValue = 3;
+    pNode1->m_list_data.m_ch = 'C';
+    ASSERT_EQ(DS_OK, m_List.InsertNodeAtHead(pNode1));
 
-	pNode1 = new NS_LIST::List<Demo>;
-	pNode1->m_list_data.m_iValue = 2;
-	pNode1->m_list_data.m_ch = 'B';
-	ASSERT_EQ(DS_OK, m_List.InsertNodeAtCur(pCur, pNode1));
-	
-	pNode1 = new NS_LIST::List<Demo>;
-	pNode1->m_list_data.m_iValue = 4;
-	pNode1->m_list_data.m_ch = 'D';
-	ASSERT_EQ(DS_OK, m_List.InsertNodeAtCur(pCur, pNode1));
+    pNode1 = new NS_LIST::List<Demo>;
+    pNode1->m_list_data.m_iValue = 2;
+    pNode1->m_list_data.m_ch = 'B';
+    ASSERT_EQ(DS_OK, m_List.InsertNodeAtCur(pCur, pNode1));
+    
+    pNode1 = new NS_LIST::List<Demo>;
+    pNode1->m_list_data.m_iValue = 4;
+    pNode1->m_list_data.m_ch = 'D';
+    ASSERT_EQ(DS_OK, m_List.InsertNodeAtCur(pCur, pNode1));
 
-	NS_LIST::List<Demo> *pTemp = nullptr;
-	ASSERT_EQ(DS_OK, m_List.GetCurNode(pTemp));
-	ASSERT_EQ(3, pTemp->m_list_data.m_iValue);
-	ASSERT_EQ(DS_OK, m_List.GetCurNode(pTemp));
-	ASSERT_EQ(1, pTemp->m_list_data.m_iValue);
-	ASSERT_EQ(DS_OK, m_List.GetCurNode(pTemp));
-	ASSERT_EQ(4, pTemp->m_list_data.m_iValue);
-	ASSERT_EQ(DS_OK, m_List.GetCurNode(pTemp));
-	ASSERT_EQ(2, pTemp->m_list_data.m_iValue);
+    NS_LIST::List<Demo> *pTemp = nullptr;
+    ASSERT_EQ(DS_OK, m_List.GetCurNode(pTemp));
+    ASSERT_EQ(3, pTemp->m_list_data.m_iValue);
+    ASSERT_EQ(DS_OK, m_List.GetCurNode(pTemp));
+    ASSERT_EQ(1, pTemp->m_list_data.m_iValue);
+    ASSERT_EQ(DS_OK, m_List.GetCurNode(pTemp));
+    ASSERT_EQ(4, pTemp->m_list_data.m_iValue);
+    ASSERT_EQ(DS_OK, m_List.GetCurNode(pTemp));
+    ASSERT_EQ(2, pTemp->m_list_data.m_iValue);
 }
--- include/List.h
+++ include/List.h
@@ -7,120 +7,120 @@
 
 namespace NS_LIST
 {
-	template <typename T>
-	class List
-	{
-		public:
-			/**
-			 * @brief 构造函数
-			 */
-			List() : m_next_ptr(nullptr), m_list_len(0)
-			{
-			}
+    template <typename T>
+    class List
+    {
+        public:
+            /**
+             * @brief 构造函数
+             */
+            List() : m_next_ptr(nullptr), m_list_len(0)
+            {
+            }
 
-			/**
-			 * @brief 析构函数
-			 */
-			~List()
-			{}
+            /**
+             * @brief 析构函数
+             */
+            ~List()
+            {}
 
-			/**
-			 * @brief 头插
-			 *
-			 * @prama pNode 插入节点
-			 *
-			 * @return  DS_OK 成功; DS_FAIL 失败
-			 */
-			int32_t InsertNodeAtHead(List<T> *&pNode)
-			{
-				if (nullptr == pNode) {
-					return DS_FAIL;
-				}
+            /**
+             * @brief 头插
+             *
+             * @prama pNode 插入节点
+             *
+             * @return  DS_OK 成功; DS_FAIL 失败
+             */
+            int32_t InsertNodeAtHead(List<T> *&pNode)
+            {
+                if (nullptr == pNode) {
+                    return DS_FAIL;
+                }
 
-				NS_LIST::List<T> *pTemp = m_next_ptr;
-				m_next_ptr = pNode;
-				pNode->m_next_ptr = pTemp;
+                NS_LIST::List<T> *pTemp = m_next_ptr;
+                m_next_ptr = pNode;
+                pNode->m_next_ptr = pTemp;
 
-				++m_list_len;
-				return DS_OK;
-			}
+                ++m_list_len;
+                return DS_OK;
+            }
 
-			/**
-			 * @brief 当前节点后插入节点
-			 *
-			 * @prama pNode 插入节点, pCur 当前节点
-			 *
-			 * @return  DS_OK 成功; DS_FAIL 失败
-			 */
-			int32_t InsertNodeAtCur(List<T> *&pCur, List<T> *&pNode)
-			{
-				if (nullptr == pNode || nullptr == pCur) 
-				{
-					return DS_FAIL;
-				}
+            /**
+             * @brief 当前节点后插入节点
+             *
+             * @prama pNode 插入节点, pCur 当前节点
+             *
+             * @return  DS_OK 成功; DS_FAIL 失败
+             */
+            int32_t InsertNodeAtCur(List<T> *&pCur, List<T> *&pNode)
+            {
+                if (nullptr == pNode || nullptr == pCur) 
+                {
+                    return DS_FAIL;
+                }
 
-				NS_LIST::List<T> *pTemp = pCur->m_next_ptr;
-				pCur->m_next_ptr = pNode;
-				pNode->m_next_ptr = pTemp;
+                NS_LIST::List<T> *pTemp = pCur->m_next_ptr;
+                pCur->m_next_ptr = pNode;
+                pNode->m_next_ptr = pTemp;
 
-				++m_list_len;
-				return DS_OK;
-			}
+                ++m_list_len;
+                return DS_OK;
+            }
 
-			/**
-			 * @brief 删除当前节点后一个节点
-			 *
-			 * @prama pCur 当前节点
-			 *
-			 * @return  DS_OK 成功; DS_FAIL 失败
-			 */
-			int32_t DeleteCurNode(List<T> *&pCur, List<T> *&pDel)
-			{
-				if (nullptr == pCur) {
-					return DS_FAIL;
-				}
+            /**
+             * @brief 删除当前节点后一个节点
+             *
+             * @prama pCur 当前节点
+             *
+             * @return  DS_OK 成功; DS_FAIL 失败
+             */
+            int32_t DeleteCurNode(List<T> *&pCur, List<T> *&pDel)
+            {
+                if (nullptr == pCur) {
+                    return DS_FAIL;
+                }
 
-				pDel = pCur->m_next_ptr;
-				pCur->m_next_ptr = pCur->m_next_ptr->m_next_ptr;
+                pDel = pCur->m_next_ptr;
+                pCur->m_next_ptr = pCur->m_next_ptr->m_next_ptr;
 
-				--m_list_len;
-				return DS_OK;
-			}
+                --m_list_len;
+                return DS_OK;
+            }
 
-			/**
-			 * @brief 获取当前节点后面一个节点
-			 *
-			 * @prama pCur 当前节点
-			 *
-			 * @return DS_OK 成功; DS_FAIL 失败 
-			 */
-			int32_t GetCurNode(List<T> *&pCur)
-			{
-				if (nullptr == pCur) {
-					pCur = m_next_ptr;
-					return DS_OK;
-				}
+            /**
+             * @brief 获取当前节点后面一个节点
+             *
+             * @prama pCur 当前节点
+             *
+             * @return DS_OK 成功; DS_FAIL 失败 
+             */
+            int32_t GetCurNode(List<T> *&pCur)
+            {
+                if (nullptr == pCur) {
+                    pCur = m_next_ptr;
+                    return DS_OK;
+                }
 
-				pCur = pCur->m_next_ptr;
-				return DS_OK;
-			}
+                pCur = pCur->m_next_ptr;
+                return DS_OK;
+            }
 
-		public:
-			/**
-			 * @brief 数据元素
-			 */ 
-			T m_list_data;
+        public:
+            /**
+             * @brief 数据元素
+             */ 
+            T m_list_data;
 
-			/**
-			 * @brief 后向指针
-			 */
-			NS_LIST::List<T> *m_next_ptr;
-			
-			/**
-			 * @brief 单链表长度
-			 */
-			uint16_t m_list_len;
-	};
+            /**
+             * @brief 后向指针
+             */
+            NS_LIST::List<T> *m_next_ptr;
+            
+            /**
+             * @brief 单链表长度
+             */
+            uint16_t m_list_len;
+    };
 }
 
 #endif
